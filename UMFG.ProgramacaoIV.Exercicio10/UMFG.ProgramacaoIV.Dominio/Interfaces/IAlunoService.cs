c767f86c8aa4a0f5238f23a54071e48d6beb1e90
VictorSales00 <140462889+VictorSales00@users.noreply.github.com>
2024-02-22 13:12:55 -0300

    Add files via upload

---
+++ UMFG.ProgramacaoIV.Exercicio10/UMFG.ProgramacaoIV.Dominio/Interfaces/IAlunoService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UMFG.ProgramacaoIV.Dominio.Entities;
+
+namespace UMFG.ProgramacaoIV.Dominio.Interfaces
+{
+    public interface IAlunoService
+    {
+        void adicionarAluno(Aluno aluno);
+
+        IEnumerable<Aluno> obterAlunos();
+
+        void adicionarNota(int ra, double notaProva, double notaTrabalho);
+
+        void adicionarFaltas(int ra, int numeroFaltas);
+
+    }
+}
---
+++ UMFG.ProgramacaoIV.Exercicio10/UMFG.ProgramacaoIV.Dominio/Services/AlunoService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UMFG.ProgramacaoIV.Dominio.Entities;
+using UMFG.ProgramacaoIV.Dominio.Interfaces;
+
+namespace UMFG.ProgramacaoIV.Dominio.Services
+{
+    public class AlunoService : IAlunoService
+    {
+
+        private readonly List<Aluno> _alunos = new List<Aluno>();
+
+        public void adicionarAluno(Aluno aluno) => _alunos.Add(aluno);
+
+        public void adicionarNota(int ra, double notaProva, double notaTrabalho) 
+        {
+            Aluno aluno = _alunos.Find(a => a.RA == ra);
+
+            if (aluno != null)
+            {
+                aluno.NotaTrabalho = notaTrabalho;
+                aluno.NotaProva = notaProva;
+                Console.WriteLine("|          Nota lançada          |");
+            }
+            else 
+            {
+                Console.WriteLine("**       Aluno não encontrado    **");
+            }
+        }
+
+        public void adicionarFaltas(int ra, int numeroFaltas)
+        {
+            Aluno aluno = _alunos.Find(a => a.RA == ra);
+
+            if (aluno != null)
+            {
+                aluno.Faltas = numeroFaltas;
+                
+                Console.WriteLine("|       Falta(s) Lançada(s)      |");
+            }
+            else
+            {
+                Console.WriteLine("**       Aluno não encontrado     **|");
+            }
+        }
+
+        public IEnumerable<Aluno> obterAlunos() => _alunos;
+
+        
+    }
+}
